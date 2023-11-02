d315a628f3d85618317234be7d9f502e7063a9e2
wx <2841169257@qq.com>
2023-11-02 15:02:44 +0800

    新增:Q9 写的有点复杂了

--- leecode/src/main/java/ArrayList/Q9.java
+++ leecode/src/main/java/ArrayList/Q9.java
@@ -1,6 +1,7 @@
 package ArrayList;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 /**
  * @className: Q9
@@ -9,39 +10,40 @@ import java.util.Arrays;
  **/
 
 public class Q9 {
-    //    9. 回文数
-//    提示
-//    给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
-//
-//    回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
-//
-//    例如，121 是回文，而 123 不是。
-//
-//
-//    示例 1：
-//
-//    输入：x = 121
-//    输出：true
-//    示例 2：
-//
-//    输入：x = -121
-//    输出：false
-//    解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
-//    示例 3：
-//
-//    输入：x = 10
-//    输出：false
-//    解释：从右向左读, 为 01 。因此它不是一个回文数。
-    public boolean isPalindrome(int x) {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-1));
+    }
+    public static boolean isPalindromeMy(int x) {
+        boolean flag = false;
         if (x < 0){
             return false;
         }
         String[] split = String.valueOf(x).split("");
-        // 如果为奇数 取出奇数标志位作为分割线前与后的数字
-        // 如果为偶数+
-        return true;
+        int length = split.length;
+        if (length == 1){
+            return true;
+        }
+        if (length == 2){
+            return Objects.equals(split[0],split[1]);
+        }
+        for (int i = 0; i < length / 2; i++) {
+            System.out.println("split[i]:" + split[i] + " " +  "split[length - i -1]:" + split[length - i -1]);
+            flag = Objects.equals(split[i],split[length - i -1]);
+            if (!flag){
+                break;
+            }
+        }
+        return flag;
     }
-    public static void main(String[] args) {
 
+    public static boolean isPalindrome(int x) {
+        Integer num=x;
+        String str=num.toString();
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i)!=str.charAt(str.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
     }
 }
